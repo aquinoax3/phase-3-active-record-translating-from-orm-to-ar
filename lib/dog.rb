cacbd942b26b6e4469d171a45979892c5f78ab79
Axel Aquino <aquino.axel@gmail.com>
2022-03-18 13:57:48 -0400

    commit tree ed556579458bbf7596d074f2ed862ec123882006

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,19 @@
-class Dog
+ActionRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/dog.sqlite"
+)
+
+sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dog (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+    )
+SQL
+ActiveRecord::Base.connection.execute(sql)
+
+
+
+
+class Dog < ActionRecord::Base
 end
